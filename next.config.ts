529eb7f2687232616d43e15984e6c7f5978755e5
Ihsan76 <103315435+Ihsan76@users.noreply.github.com>
2026-01-22 17:54:16 +0300

    Add ignoreBuildErrors to TypeScript config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    typescript: { ignoreDevErrors: true },
-    eslint: { ignoreDuringBuilds: true },
+typescript: { ignoreDevErrors: true, tsconfigPath: false, ignoreBuildErrors: true },    eslint: { ignoreDuringBuilds: true },
     productionBrowserSourceMaps: false,
 };
 
