eb0e3025ef9d89d5343853d2c65513fa4a11c5cd
Ihsan76 <103315435+Ihsan76@users.noreply.github.com>
2026-01-22 17:43:49 +0300

    Disable TypeScript and ESLint checks during build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    typescript: { ignoreDevErrors: true },
+    eslint: { ignoreDuringBuilds: true },
 };
 
 export default nextConfig;
